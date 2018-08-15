2031733cf33f30fc7a45fe23a5bd428541982a04
subramani simhachalam <iceapple@subramanis-iMac.local>
2018-08-15 22:14:48 +0530

    Added Parallax Background

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,24 @@
     
     <v-content>
       <section>
-        <v-parallax src="../src/assets/hero.jpeg" height="600">
+        <v-parallax :src="images.hero" height="600">
           <v-layout
             column
             align-center
             justify-center
             class="white--text"
           >
-            <img src="assets/vuetify.png" alt="Vuetify.js" height="200">
-            <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
-            <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
-            <v-btn
+            <!-- <img :src="images.vuetify" alt="Vuetify.js" height="200"> -->
+            <h1 class="white--text mb-2 display-1 text-xs-center">Subramani Simhachalam</h1>
+            <div class="subheading mb-3 text-xs-center">Personal Blog</div>
+            <!-- <v-btn
               class="blue lighten-2 mt-5"
               dark
               large
               href="/pre-made-themes"
             >
               Get Started
-            </v-btn>
+            </v-btn> -->
           </v-layout>
         </v-parallax>
       </section>
@@ -95,7 +95,7 @@
       </section>
 
       <section>
-        <v-parallax src="assets/section.jpg" height="380">
+        <v-parallax :src="images.section" height="380">
           <v-layout column align-center justify-center>
             <div class="headline white--text mb-3 text-xs-center">Web development has never been easier</div>
             <em>Kick-start your application today</em>
@@ -189,6 +189,12 @@ export default {
   data() {
       return {
         title: 'Blog App',
+        images: {
+                hero: require('./assets/hero.jpg'),
+                section: require('./assets/section.jpg'),
+                vuetify: require('./assets/vuetify.png')
+                //sample: require('./assets/hero.jpg')
+            },
         posts: [
             {
               title: 'Fusce ullamcorper tellus',
